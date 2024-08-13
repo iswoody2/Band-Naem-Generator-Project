974b48dc7949caac9d7bfda512d0fdd6a589a255
IanHunter_LawCode <153140620+iswoody2@users.noreply.github.com>
2024-08-13 20:18:00 +0100

    Add files via upload

---
+++ Bandname_generator.py
@@ -0,0 +1,7 @@
+
+print("Welcome to the Band Name Generator Project!!")
+city_name = input("What city did you grew up in? ")
+
+pet_name = input("What is the name of your pet? ")
+
+print("Your Bandname is " + city_name + " " + pet_name)
